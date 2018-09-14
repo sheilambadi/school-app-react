fcd38281209edca24d96e6dcd2fa17d76a023131
sheilambadi <mbadiatieno@gmail.com>
2018-09-14 09:25:28 +0300

    Add navbar

--- Angular/movies-app/src/app/app.module.ts
+++ Angular/movies-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import  { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { MovieComponent } from './movie/movie.component';
 import { MovieService } from './services/movie.service';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MovieComponent
+    MovieComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
